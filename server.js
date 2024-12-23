1df0f88adced075dfa9c650a848bd1630733ce96
Aman <kulaman20903@gmail.com>
2024-12-23 23:49:39 +0530

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000; // Use process.env.PORT for Vercel;
 
 // Enable CORS
 app.use(cors());
